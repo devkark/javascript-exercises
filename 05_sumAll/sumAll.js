b1a7e558ec52debbacda47227be321e6c5f412ff
devkark <90148575+devkark@users.noreply.github.com>
2024-10-26 00:43:06 +0200

    Solutions for the first seven js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  if (min < 0 || max < 0) return "ERROR";
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min > max) {
+    const tmp = max;
+    max = min;
+    min = tmp;
+  }
 
+  let sum = 0;
+  for (min; min <= max; min++) {
+    sum += min;
+  }
+  return sum;
 };
 
 // Do not edit below this line
